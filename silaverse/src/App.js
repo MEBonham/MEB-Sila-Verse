9e97ee8b188c25493ad507549cdb7c165533a854
MEBonham <programmer@mckaybonham.com>
2019-05-17 17:59:20 -0600

    Delete Hero implemented (but not then() follow-up)

--- silaverse/src/App.js
+++ silaverse/src/App.js
@@ -4,6 +4,7 @@ import { Route } from 'react-router-dom';
 import Header from './components/auth/Header';
 import Sidebar from './components/sidebar/Sidebar';
 import CharSheet from './components/main/CharSheet';
+import EditHeroForm from './components/main/EditHeroForm';
 import NewHeroForm from './components/main/NewHeroForm';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
@@ -18,6 +19,8 @@ function App() {
       <Route exact path="/" component={Sidebar} />
       <Route path="/viewhero" component={Sidebar} />
       <Route path="/viewhero/:id" component={CharSheet} />
+      <Route path="/edithero" component={Sidebar} />
+      <Route path="/edithero/:id" component={EditHeroForm} />
       <Route path="/newhero" component={Sidebar} />
       <Route path="/newhero" component={NewHeroForm} />
       <Route path="/login" component={Login} />
--- silaverse/src/components/main/EditHeroForm.js
+++ silaverse/src/components/main/EditHeroForm.js
@@ -3,9 +3,13 @@ import useForm from '../../hooks/useForm';
 import firebase from '../../config/fbConfig';
 import { useGlobal } from 'reactn';
 
-const NewHeroForm = () => {
+import deleteicon from '../../images/delete-icon.png';
+
+const EditHeroForm = props => {
 
     const [ prevHeroes, setHeroes ] = useGlobal('heroes');
+    const urlid = props.match.params.id;
+
     const sendInfo = () => {
         const db = firebase.firestore();
         if (!inputs.identity) {
@@ -37,20 +41,40 @@ const NewHeroForm = () => {
         .catch(err => {
             console.error("Error adding hero: ", err);
         });
-        setInputs({
-            ...inputs,
-            urlid: "",
-            name: "",
-            identity: ""
-        })
+    }
+
+    const handleDelete = () => {
+        if (window.confirm("Are you sure you want to delete this hero?")) {
+            const db = firebase.firestore();
+            db.collection("heroes").where("urlid", "==", urlid)
+                .get()
+                .then(querySnapshot => {
+                    if (!querySnapshot.empty) {
+                        const heroId = querySnapshot.docs[0].id;
+                        db.collection("heroes").doc(heroId)
+                            .delete()
+                            .then()
+                            .catch(err => {
+                                console.log("Error deleting hero: ", err);
+                            });
+                    }
+                    else {
+                        console.log("Cannot find hero matching this page.");
+                    }
+                })
+                .catch(err => {
+                    console.log("Error getting hero that goes with this page in order to delete: ", err);
+                });
+        }
     }
 
     const { inputs, handleInputChange, handleSubmit, setInputs } = useForm(sendInfo);
 
     return(
         <section className="hero-info-form-envelope">
+            <img src={deleteicon} alt="Delete Hero" className="push-right" onClick={handleDelete} />
             <form className="hero-info-form" onSubmit={handleSubmit}>
-                <h1>New Hero Info</h1>
+                <h1>Edit Hero Info</h1>
                 <label htmlFor="urlid">URL ID</label>
                 <input
                     type="text"
@@ -80,4 +104,4 @@ const NewHeroForm = () => {
     );
 }
 
-export default NewHeroForm;
+export default EditHeroForm;
